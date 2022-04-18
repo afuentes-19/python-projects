e7590915a3203fe117b04cb9a3ca80f942f6140b
afuentes-19 <fuentes.j.alan@gmail.com>
2022-04-18 18:59:39 -0400

    first commit

---
+++ mad_libs.py
@@ -0,0 +1,16 @@
+print("Welcome to mad libs")
+noun1 = input("Enter a noun:")
+adjective1 = input ("Enter an adjective:")
+verb1 = input("Enter a verb:")
+name1 = input("Enter a name:")
+place = input("Enter a place:")
+place2 = input("Enter another place:")
+food = input("Enter a food:")
+weather = input("Enter a word that describes the weather:")
+time_of_year = input("Enter the time of year:")
+animal = input("Enter the name of an animal:")
+
+print ("On a " + weather + " " + time_of_year + "day, " +
+    " " + name1 + " was walking around the " + place + " and then suddenly saw a " +
+    animal + " " + verb1 + "towards " + noun1 + " in the middle of the " + place2 + "leading " + name1 + " to suddenly " +
+    "crave for a " + food)
