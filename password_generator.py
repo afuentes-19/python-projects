b5e8b4937c1c99d270d87242f8796a9fc8715f1e
afuentes-19 <fuentes.j.alan@gmail.com>
2022-05-30 17:34:09 -0400

    fix: enforce password length constraint

--- password_generator.py
+++ password_generator.py
@@ -2,6 +2,7 @@
 
 import string
 import random 
+import sys
 
 lower_characters = list(string.ascii_lowercase)
 upper_characters = list(string.ascii_uppercase)
@@ -14,6 +15,12 @@ print("Welcome to the MyPassword Generator")
 n_chars = int(input("How many letters would you like in your password?\n"))
 n_numbers = int(input("How many numbers would you like?\n"))
 n_symbols = int(input("How many symbols would you like?\n"))
+
+#enforce constraints 
+if (n_chars + n_numbers + n_symbols > 50): 
+    print("Password is too long. Please run the program to try again.")
+    sys.exit()
+
 for n in range(0,n_chars):
     case = random.choice([lower_characters, upper_characters])
     letter = random.choice(case)
