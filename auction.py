20820831950d8fd13d3b8f13e172dc003b0f159d
afuentes-19 <fuentes.j.alan@gmail.com>
2022-12-01 19:41:19 -0500

    feature: add auction program!

---
+++ auction.py
@@ -0,0 +1,38 @@
+import os 
+import random 
+from lists import auctionItems 
+
+def addBidder(name, bidAmt):
+    bidders[name] = bidAmt
+
+def determineWinner(bidders):
+    highestBid = 0
+    result = ""
+    for name in bidders:
+        print(f"Currently looking at {name} with the bid: {bidders[name]}\n")
+        if bidders[name] > highestBid:
+            highestBid = bidders[name]
+            result = f"The winning bid is by {name} who wins today's item, {todaysItem}, for ${bidders[name]}. Congratulations!\n"
+        elif bidders[name] == highestBid:
+            result = "There is a tie"
+    return result 
+
+bidders = {}
+moreBidders = True
+todaysItem = random.choice(auctionItems)
+print("Hello! Welcome to the Auction Game!")
+
+while moreBidders: 
+    print(f"Today's bid is for this item: {todaysItem}")
+    bidderName = input("What is your name?\n")
+    bidderAmt = int(input(f"Welcome to the auction, {bidderName}! How much would you like to bid? (Enter number amount only)\n"))
+    nextBidder = input("Is there another bidder? Please enter 'yes' or 'no'.\n")
+    addBidder(bidderName, bidderAmt)
+    if nextBidder == 'no':
+        moreBidders = False
+    os.system('cls')
+
+print(determineWinner(bidders))
+
+
+
